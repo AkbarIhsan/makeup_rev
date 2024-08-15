03423e126f48172429dc8752efd930ab5d9ea900
AkbarIhsan <93473539+AkbarIhsan@users.noreply.github.com>
2024-08-15 12:01:30 +0700

    update tampilan register mua

--- FE/components/layout/formreg.vue
+++ FE/components/layout/formreg.vue
@@ -2,33 +2,57 @@
     <div>
         <div class="w-full flex flex-col">
             <div class="w-full flex justify-center">
-                <h1 class="text-2xl font-semibold mt-5">Sign Up</h1>
+                <h1 class="text-2xl font-semibold mt-4">Sign Up</h1>
             </div>
             <form class="mx-10 mt-5 flex flex-col gap-2" action="">
+                <!-- name -->
                 <div>
                     <label id="inputLabelUs" class="text-figma-11 font-semibold text-sm transition duration-300 opacity-0" for="inputFieldUs">Name</label>
                     <input id="inputFieldUs" class="w-full h-11 border px-3 text-figma-11 font-thin rounded-lg" placeholder="Enter your name" type="text">
                 </div>
+
+                <!-- email address -->
                 <div>
                     <label id="inputLabelEm" class="text-figma-11 font-semibold text-sm transition duration-300 opacity-0" for="inputFieldEm">Email Address</label>
                     <input id="inputFieldEm" class="w-full h-11 border px-3 text-figma-11 font-thin rounded-lg" placeholder="Enter your email" type="text">
                 </div>
+
+                <!-- password -->
                 <div>
                     <label id="inputLabelPas" class="text-figma-11 font-semibold text-sm transition duration-300 opacity-0" for="inputFieldPas">Password</label>
-                    <input id="inputFieldPas" class="w-full h-11 border px-3 text-figma-11 font-thin rounded-lg" placeholder="Enter your password" type="password">
+                    <div class="relative w-full">
+                        <div class="absolute inset-y-0 right-0 flex items-center px-2">
+                            <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
+                            <label class="py-1 text-sm cursor-pointer js-password-label" for="toggle"><img class="h-4 lg:h-5 md:h-4 sm:h-4 w-4 lg:w-5 md:w-4 sm:w-4" src="https://i.ibb.co.com/Wp1WFhf/eye-password-see-view-svgrepo-com.png" alt=""></label>
+                        </div>
+                        <input id="inputFieldPas" class="w-full h-11 border px-3 text-figma-11 font-thin rounded-lg js-password" placeholder="Enter your password" type="password">
+                    </div>
                 </div>
+
+                <!-- confirm password -->
                 <div>
                     <label id="inputLabelConPas" class="text-figma-11 font-semibold text-sm transition duration-300 opacity-0" for="inputFieldConPas">Confirm Password</label>
-                    <input id="inputFieldConPas" class="w-full h-11 border px-3 text-figma-11 font-thin rounded-lg" placeholder="Enter again your password" type="password">
+                    <div class="relative w-full">
+                        <div class="absolute inset-y-0 right-0 flex items-center px-2">
+                            <input class="hidden js-confirm-password-toggle" id="confirm-toggle" type="checkbox" />
+                            <label class="py-1 text-sm cursor-pointer js-confirm-password-label" for="confirm-toggle">
+                                <img class="h-4 lg:h-5 md:h-4 sm:h-4 w-4 lg:w-5 md:w-4 sm:w-4" src="https://i.ibb.co.com/Wp1WFhf/eye-password-see-view-svgrepo-com.png" alt="">
+                            </label>
+                        </div>
+                        <input id="inputFieldConPas" class="w-full h-11 border px-3 text-figma-11 font-thin rounded-lg js-confirm-password" placeholder="Enter again your password" type="password">
+                    </div>
                 </div>
-                <div>
+
+                <!-- terms and conditions -->
+                <div class="mt-2">
                     <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 accent-figma-7 border border-black rounded cursor-pointer ">
                     <label for="terms" class="ml-2 text-figma-11 opacity-30 cursor-pointer">I accept the <a class="font-semibold text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                 </div>
-                <div class="w-full flex justify-center">
+
+                <!-- sign up button -->
+                <div class="w-full flex justify-center items-center mt-4">
                     <button class="bg-figma-20 text-figma-19 py-3 px-16 rounded-3xl text-sm cursor-pointer hover:bg-opacity-80">Sign Up</button>
                 </div>
-
             </form>
         </div>
     </div>
@@ -100,6 +124,31 @@
                     inputLabelConPas.classList.add('opacity-0');
                     inputFieldConPas.setAttribute('placeholder','Enter your password');
                 }
+            });
+
+            const passwordToggle = document.querySelector('.js-password-toggle');
+            const confirmPasswordToggle = document.querySelector('.js-confirm-password-toggle');
+
+            function togglePasswordVisibility(inputField, label) {
+                if (inputField.type === 'password') {
+                    inputField.type = 'text';
+                    label.innerHTML = '<img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/B6jkXf1/eye-key-look-password-security-see-svgrepo-com.png" alt="">';
+                } else {
+                    inputField.type = 'password';
+                    label.innerHTML = '<img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/Wp1WFhf/eye-password-see-view-svgrepo-com.png" alt="">';
+                }
+            }
+
+            passwordToggle.addEventListener('change', function() {
+                const inputFieldPas = document.querySelector('.js-password');
+                const passwordLabel = document.querySelector('.js-password-label');
+                togglePasswordVisibility(inputFieldPas, passwordLabel);
+            });
+
+            confirmPasswordToggle.addEventListener('change', function() {
+                const inputFieldConPas = document.querySelector('.js-confirm-password');
+                const confirmPasswordLabel = document.querySelector('.js-confirm-password-label');
+                togglePasswordVisibility(inputFieldConPas, confirmPasswordLabel);
             });
         }
     };
