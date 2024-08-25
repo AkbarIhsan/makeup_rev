c8b82fea0c6f9b7a21ac61958f210e1407799c0f
AkbarIhsan <93473539+AkbarIhsan@users.noreply.github.com>
2024-08-25 19:01:40 +0700

    fix nested nuxtlink di cardprofilemua

--- FE/layouts/default.vue
+++ FE/layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
-    <div>
-        <Navbar/>
-        <div class="w-full h-auto flex flex-col gap-y-28 px-20 bg-figma-4 overflow-hidden">
-            <slot />
-        </div>
+    <Navbar/>
+    <div class="w-full h-auto flex flex-col gap-y-28 px-20 bg-figma-4 overflow-hidden">
+        <slot />
+        <Footer/>
     </div>
 </template>
