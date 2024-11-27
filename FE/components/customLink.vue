a9ca1fb5535ece693bfb80a0044a0edc1bbd38b1
AkbarIhsan <93473539+AkbarIhsan@users.noreply.github.com>
2024-11-27 14:39:45 +0700

    update : customLink

--- FE/components/customLink.vue
+++ FE/components/customLink.vue
@@ -3,7 +3,7 @@
         :to="to"
         :class="{
             'bg-figma-12 text-figma-1 py-3 px-8 rounded-3xl cursor-pointer hover:bg-opacity-80': variant === 'primary',
-            'bg-figma-20 text-figma-11 py-3 px-8 rounded-3xl cursor-pointer hover:bg-opacity-70': variant === 'secondary',
+            'bg-figma-20 text-figma-11 md:py-3 md:px-8 py-2 px-4 rounded-3xl text-xs md:text-sm cursor-pointer hover:bg-opacity-70': variant === 'secondary',
             'bg-transparent hover:bg-figma-5 border-2 border-figma-1 font-semibold text-figma-1 py-3 px-8 rounded-3xl cursor-pointer hover:bg-opacity-80': variant === 'transparent',
             }">
         <slot/>
--- FE/pages/service.vue
+++ FE/pages/service.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="w-full h-screen flex">
-        <div class="w-full h-full flex justify-center items-center relative">
-            <img class="w-96 h-96" src="@/assets/bg-efek.png" alt="">
-            <img class="absolute w-[19rem] h-[19rem] rounded-full" src="@/assets/img-service.png" alt="">
+    <div class="w-full h-screen flex flex-col md:flex-row">
+        <div class="w-full h-full flex justify-center -bottom-24 md:-bottom-0 items-center relative">
+            <img class="md:w-96 md:h-96 w-64 h-64" src="@/assets/bg-efek.png" alt="">
+            <img class="absolute md:w-[19rem] md:h-[19rem] w-48 h-48 rounded-full" src="@/assets/img-service.png" alt="">
         </div>
-        <div class="w-full h-full flex justify-center items-center">
-            <div class="md:text-4xl font-bold flex w-full flex-wrap justify-center gap-2 text-white text-shadow-md">
+        <div class="w-full h-full flex justify-center pb-12 items-center">
+            <div class="md:text-4xl text-2xl font-bold flex w-full flex-wrap justify-center gap-2 text-white text-shadow-md">
                 <div>Discover your</div> 
                 <div class="text-figma-12">favourite</div>
                 <div class="text-figma-12">makeup artist</div>
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="w-full h-auto flex flex-col">
-        <div class="w-full flex justify-between text-2xl">
+        <div class="w-full flex justify-between text-xl md:text-2xl">
             <h1 class="text-figma-7 font-bold">Gallery Makeup Look</h1>
             <NuxtLink class="text-figma-12 font-semibold" to="">View More</NuxtLink>
         </div>
@@ -26,7 +26,7 @@
                 <h1 class="text-figma-7 text-2xl font-bold">Makeup Artist</h1>
                 <NuxtLink class="flex items-center text-figma-12 font-semibold text-xl" to="/mua">View More</NuxtLink>
             </div>
-            <div class="flex justify-between gap-y-12 flex-wrap mt-12">
+            <div class="lg:columns-4 md:columns-2 columns-2 space-y-10 md:space-y-4 mt-12">
                 <Cardprofilemua/>
                 <Cardprofilemua/>
                 <Cardprofilemua/>
